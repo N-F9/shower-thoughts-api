3a00b8fa9789c17340bca9a3099691f7dc86245a
Nick Ferguson <ndferguson@protonmail.com>
2023-12-15 13:39:02 -0500

    api errors

--- web/src/main/java/me/nickf/web/ThoughtsController.java
+++ web/src/main/java/me/nickf/web/ThoughtsController.java
@@ -2,9 +2,12 @@ package me.nickf.web;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,8 +22,17 @@ public class ThoughtsController {
 	}
 
 	@GetMapping("/thoughts/{id}")
-	public Optional<Thoughts> getThoughtById(@PathVariable(value = "id") String id) {
-		return this.thoughtsRepository.findById(id);
+	public ResponseEntity<Object> getThoughtById(@PathVariable(value = "id") String id) {
+		Optional<Thoughts> thought = this.thoughtsRepository.findById(id);
+		
+		if(thought.equals(Optional.empty())) {
+			HashMap<String, String> m = new HashMap<>();
+			m.put("code", "422");
+			m.put("message", "The id variable is invalid, please provide a valid id.");
+			return new ResponseEntity<>(m, HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+
+		return new ResponseEntity<>(thought, HttpStatus.OK);
 	}
 
 	@GetMapping("/thoughts/random")
@@ -43,19 +55,70 @@ public class ThoughtsController {
 	}
 
   @GetMapping("/thoughts")
-  public Iterable<Thoughts> findAllThoughts(
+  // public Iterable<Thoughts> findAllThoughts(
+	public ResponseEntity<Object> findAllThoughts(
 		@RequestParam(value = "author", defaultValue = "", required = true) String author,
 		@RequestParam(value = "over_18", defaultValue = "false", required = true) String over_18,
 		@RequestParam(value = "from", defaultValue = "", required = true) String from,
 		@RequestParam(value = "to", defaultValue = "", required = true) String to,
 		@RequestParam(value = "limit", defaultValue = "10", required = true) String limit
 	) {
+		over_18 = over_18.toLowerCase();
+		if (!(over_18.equals("true") || over_18.equals("false"))) {
+			HashMap<String, String> m = new HashMap<>();
+			m.put("code", "422");
+			m.put("message", "The over_18 parameter is invalid. Please either use 'true' or 'false'.");
+			return new ResponseEntity<>(m, HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+
+		int limitInt = 10;
+
+		try {
+			limitInt = Integer.parseInt(limit);
+		} catch (Exception e) {
+			HashMap<String, String> m = new HashMap<>();
+			m.put("code", "400");
+			m.put("message", "The limit parameter is invalid. Please an integer ranging from 1 to 1000.");
+			return new ResponseEntity<>(m, HttpStatus.BAD_REQUEST);
+		}
+		if (limitInt < 1) limitInt = 1;
+		if (limitInt > 1000) limitInt = 1000;
+
+		Date fromDate = null;
+		Date toDate = null;
+
+		if (!from.equals("")) {
+			try {
+				fromDate = Date.valueOf(from);
+			} catch (Exception e) {
+				HashMap<String, String> m = new HashMap<>();
+				m.put("code", "422");
+				m.put("message", "The from date is in an invalid format. (YYYY-MM-DD)");
+				return new ResponseEntity<>(m, HttpStatus.UNPROCESSABLE_ENTITY);
+			}
+		}
+
+		if (!to.equals("")) {
+			try {
+				toDate = Date.valueOf(to);
+			} catch (Exception e) {
+				HashMap<String, String> m = new HashMap<>();
+				m.put("code", "422");
+				m.put("message", "The to date is in an invalid format. (YYYY-MM-DD)");
+				return new ResponseEntity<>(m, HttpStatus.UNPROCESSABLE_ENTITY);
+			}
+		}
+
+		if (fromDate != null && toDate != null && fromDate.compareTo(toDate) > 0) {
+			HashMap<String, String> m = new HashMap<>();
+			m.put("code", "422");
+			m.put("message", "The to date must be later than or equal to the from date.");
+			return new ResponseEntity<>(m, HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+
 		ArrayList<Thoughts> t = new ArrayList<>();
 		Iterator<Thoughts> it = this.thoughtsRepository.findAllByOrderByCreatedDesc().iterator();
 
-		int limitInt = Integer.parseInt(limit);
-		if (limitInt < 1) limitInt = 1;
-		if (limitInt > 1000) limitInt = 1000;
 
 		while(it.hasNext() && t.size() < limitInt) {
 			Thoughts thought = it.next();
@@ -66,17 +129,6 @@ public class ThoughtsController {
 
 			if (!over_18.equals(thought.getOver_18() + "")) {
 				continue;
-			}
-
-			Date fromDate = null;
-			Date toDate = null;
-
-			if (!from.equals("")) {
-				fromDate = Date.valueOf(from);
-			}
-
-			if (!to.equals("")) {
-				toDate = Date.valueOf(to);
 			}
 
 			Date td = thought.getCreated();
@@ -102,6 +154,6 @@ public class ThoughtsController {
 			t.add(thought);
 		}
 
-		return t;
+		return new ResponseEntity<>(t, HttpStatus.OK);
   }
 }
