e01c2dea91f09c1a8556704153acbb8f6c61eb1a
Nick Ferguson <ndferguson@protonmail.com>
2025-01-03 16:28:49 -0500

    added docker support

--- data/src/App.java
+++ data/src/App.java
@@ -22,8 +22,8 @@ import org.json.simple.parser.JSONParser;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class App {
-	public static String url = "jdbc:postgresql://localhost:5432/showerthoughts";
-	public static String user = "postgres";
+	public static String url = System.getenv("url");
+	public static String user = System.getenv("user");
 	public static String password = System.getenv("password");
 
 	public static void main(String[] args) throws Exception {
